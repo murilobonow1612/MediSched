d53db8748282a2bb2e0e706f49948b83a7b59ba2
Murilo <murilobonow07@gmail.com>
2025-05-11 19:05:34 -0300

    feat: criação da estrutura do projeto
    -Front-end criado em React com Vite utilizando JS;
    - Backend criado com Spring Initializr;
    - Configurado com Maven e Java 17;
    - Adicionadas dependências básicas: Spring Web, Spring Data JPA, Lombok;
    - Estrutura inicial de pacotes pronta para desenvolvimento;

---
+++ backend/src/main/java/com/MediSched/Agendamento/MediSchedApplication.java
@@ -0,0 +1,13 @@
+package com.MediSched.Agendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediSchedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediSchedApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/MediSched/Agendamento/MediSchedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MediSched.Agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediSchedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
